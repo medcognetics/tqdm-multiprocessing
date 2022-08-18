6119929e9b76bbd8acdd3acabc7098ef68e0a57c
Scott Chase Waggener <tidalpaladin@protonmail.com>
2022-08-18 10:23:04 -0500

    Make ConcurrentMapper a top level import

--- tqdm_multiprocessing/__init__.py
+++ tqdm_multiprocessing/__init__.py
@@ -2,5 +2,8 @@
 # -*- coding: utf-8 -*-
 import importlib.metadata
 
+from .mapper import ConcurrentMapper
 
+
+__all__ = ["ConcurrentMapper"]
 __version__ = importlib.metadata.version("tqdm_multiprocessing")
